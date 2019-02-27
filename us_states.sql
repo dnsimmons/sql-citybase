389a656ee4ace4686e47f5d383a3f003fed0472a
Dave Simmons <dsimmonsdesign@gmail.com>
2019-02-27 18:03:35 -0500

    add sql dumps

---
+++ us_states.sql
@@ -0,0 +1,60 @@
+
+CREATE TABLE IF NOT EXISTS us_states (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  state_code char(2) NOT NULL,
+  state_title varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO us_states (id, state_code, state_title) VALUES (1, 'AL', 'Alabama');
+INSERT INTO us_states (id, state_code, state_title) VALUES (2, 'AK', 'Alaska');
+INSERT INTO us_states (id, state_code, state_title) VALUES (3, 'AZ', 'Arizona');
+INSERT INTO us_states (id, state_code, state_title) VALUES (4, 'AR', 'Arkansas');
+INSERT INTO us_states (id, state_code, state_title) VALUES (5, 'CA', 'California');
+INSERT INTO us_states (id, state_code, state_title) VALUES (6, 'CO', 'Colorado');
+INSERT INTO us_states (id, state_code, state_title) VALUES (7, 'CT', 'Connecticut');
+INSERT INTO us_states (id, state_code, state_title) VALUES (8, 'DE', 'Delaware');
+INSERT INTO us_states (id, state_code, state_title) VALUES (9, 'DC', 'District of Columbia');
+INSERT INTO us_states (id, state_code, state_title) VALUES (10, 'FL', 'Florida');
+INSERT INTO us_states (id, state_code, state_title) VALUES (11, 'GA', 'Georgia');
+INSERT INTO us_states (id, state_code, state_title) VALUES (12, 'HI', 'Hawaii');
+INSERT INTO us_states (id, state_code, state_title) VALUES (13, 'ID', 'Idaho');
+INSERT INTO us_states (id, state_code, state_title) VALUES (14, 'IL', 'Illinois');
+INSERT INTO us_states (id, state_code, state_title) VALUES (15, 'IN', 'Indiana');
+INSERT INTO us_states (id, state_code, state_title) VALUES (16, 'IA', 'Iowa');
+INSERT INTO us_states (id, state_code, state_title) VALUES (17, 'KS', 'Kansas');
+INSERT INTO us_states (id, state_code, state_title) VALUES (18, 'KY', 'Kentucky');
+INSERT INTO us_states (id, state_code, state_title) VALUES (19, 'LA', 'Louisiana');
+INSERT INTO us_states (id, state_code, state_title) VALUES (20, 'ME', 'Maine');
+INSERT INTO us_states (id, state_code, state_title) VALUES (21, 'MD', 'Maryland');
+INSERT INTO us_states (id, state_code, state_title) VALUES (22, 'MA', 'Massachusetts');
+INSERT INTO us_states (id, state_code, state_title) VALUES (23, 'MI', 'Michigan');
+INSERT INTO us_states (id, state_code, state_title) VALUES (24, 'MN', 'Minnesota');
+INSERT INTO us_states (id, state_code, state_title) VALUES (25, 'MS', 'Mississippi');
+INSERT INTO us_states (id, state_code, state_title) VALUES (26, 'MO', 'Missouri');
+INSERT INTO us_states (id, state_code, state_title) VALUES (27, 'MT', 'Montana');
+INSERT INTO us_states (id, state_code, state_title) VALUES (28, 'NE', 'Nebraska');
+INSERT INTO us_states (id, state_code, state_title) VALUES (29, 'NV', 'Nevada');
+INSERT INTO us_states (id, state_code, state_title) VALUES (30, 'NH', 'New Hampshire');
+INSERT INTO us_states (id, state_code, state_title) VALUES (31, 'NJ', 'New Jersey');
+INSERT INTO us_states (id, state_code, state_title) VALUES (32, 'NM', 'New Mexico');
+INSERT INTO us_states (id, state_code, state_title) VALUES (33, 'NY', 'New York');
+INSERT INTO us_states (id, state_code, state_title) VALUES (34, 'NC', 'North Carolina');
+INSERT INTO us_states (id, state_code, state_title) VALUES (35, 'ND', 'North Dakota');
+INSERT INTO us_states (id, state_code, state_title) VALUES (36, 'OH', 'Ohio');
+INSERT INTO us_states (id, state_code, state_title) VALUES (37, 'OK', 'Oklahoma');
+INSERT INTO us_states (id, state_code, state_title) VALUES (38, 'OR', 'Oregon');
+INSERT INTO us_states (id, state_code, state_title) VALUES (39, 'PA', 'Pennsylvania');
+INSERT INTO us_states (id, state_code, state_title) VALUES (40, 'PR', 'Puerto Rico');
+INSERT INTO us_states (id, state_code, state_title) VALUES (41, 'RI', 'Rhode Island');
+INSERT INTO us_states (id, state_code, state_title) VALUES (42, 'SC', 'South Carolina');
+INSERT INTO us_states (id, state_code, state_title) VALUES (43, 'SD', 'South Dakota');
+INSERT INTO us_states (id, state_code, state_title) VALUES (44, 'TN', 'Tennessee');
+INSERT INTO us_states (id, state_code, state_title) VALUES (45, 'TX', 'Texas');
+INSERT INTO us_states (id, state_code, state_title) VALUES (46, 'UT', 'Utah');
+INSERT INTO us_states (id, state_code, state_title) VALUES (47, 'VT', 'Vermont');
+INSERT INTO us_states (id, state_code, state_title) VALUES (48, 'VA', 'Virginia');
+INSERT INTO us_states (id, state_code, state_title) VALUES (49, 'WA', 'Washington');
+INSERT INTO us_states (id, state_code, state_title) VALUES (50, 'WV', 'West Virginia');
+INSERT INTO us_states (id, state_code, state_title) VALUES (51, 'WI', 'Wisconsin');
+INSERT INTO us_states (id, state_code, state_title) VALUES (52, 'WY', 'Wyoming');
